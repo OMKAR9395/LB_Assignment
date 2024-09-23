78a818e515dec1185bd5c5a74a889521b29c8995
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-23 10:43:47 +0530

    Assignment

---
+++ Assignment_7/01Radius_of_Circle.c
@@ -0,0 +1,8 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//  Write a program which accept radius of circle from user and calculate its area. // 
+//  Consider value of PI as 3.14. (Area = PI * Radius * Radius)                     //
+//  Input : 5.3                                                                     //
+//  Output : 88.2026                                                                //
+//  Input : 10.4                                                                    //
+//  Output : 339.6224                                                               //
+//////////////////////////////////////////////////////////////////////////////////////
