d309656bba113547cb54f3edae7c6ecb757d5add
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-26 12:06:14 +0530

    Assignment

--- Assignment_7/01Radius_of_Circle.c
+++ Assignment_7/01Radius_of_Circle.c
@@ -6,3 +6,24 @@
 //  Input : 10.4                                                                    //
 //  Output : 339.6224                                                               //
 //////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+double CircleArea(float fRadius);
+double CircleArea(float fRadius)
+{
+double Area = 0.0;
+	float PI=3.14;
+	Area=((PI*fRadius*fRadius));
+	return Area;
+}
+int main()
+{
+        float fValue = 0.0;
+        double dRet = 0.0;
+        printf("\n Enter a Radius :");
+        scanf("%f",&fValue);
+        dRet = CircleArea(fValue);
+        printf("\n Area Of Circle Is %lf",dRet);
+        getch();
+        return 0;
+}
