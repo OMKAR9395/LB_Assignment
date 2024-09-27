c4d22a55d0a318e1f94019fc950b412f5d4ca887
OMKAR9395 <omkarmohite85@gmail.com>
2024-09-27 19:13:27 +0530

    Assignment

---
+++ Assignment_7/02Rectangle.c
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////////
+//  Write a program which accept width & height of rectangle from user          //
+//  and calculate its area. (Area = Width * Height)                             //
+//  Input : 5.3 9.78                                                            //
+//  Output : 51.834                                                             //
+//////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+double RectArea(float fWidth, float fHeight)
+{
+// Logic
+}
+int main()
+{
+float fValue1 = 0.0, fValue2 = 0.0;
+double dRet = 0.0;
+printf("\n Enter Wisth :");
+scanf("%f",&fValue1);
+printf("\n Enter Height :");
+scanf("%f",&fValue2);
+dRet = RectArea(fValue1, fValue2);
+printf("\n");
+return 0;
+}
