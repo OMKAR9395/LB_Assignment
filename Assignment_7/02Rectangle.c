b6f52a744e3654284cdd2b093b81127d6e65d385
OMKAR9395 <omkarmohite85@gmail.com>
2024-10-06 15:50:13 +0530

    Assignment

--- Assignment_7/02Rectangle.c
+++ Assignment_7/02Rectangle.c
@@ -8,17 +8,34 @@
 #include<conio.h>
 double RectArea(float fWidth, float fHeight)
 {
-
+    double Area =0.0;
+    if(fWidth>0)
+    {
+        fWidth=-fWidth;
+    }
+    if (fHeight>0)
+    {
+        fHeight=-fHeight;
+    }
+    Area = fWidth*fHeight;
+    return Area;
+    
 }
 int main()
 {
-float fValue1 = 0.0, fValue2 = 0.0;
-double dRet = 0.0;
-printf("\n Enter Width :");
-scanf("%f",&fValue1);
-printf("\n Enter Height :");
-scanf("%f",&fValue2);
-dRet = RectArea(fValue1, fValue2);
-printf("\n");
-return 0;
+    float fValue1 = 0.0, fValue2 = 0.0;
+    double dRet = 0.0;
+
+        printf("\n Enter Width :");
+        scanf("%f",&fValue1);
+
+        printf("\n Enter Height :");
+        scanf("%f",&fValue2);
+
+    dRet = RectArea(fValue1, fValue2);
+
+        printf("\n Area of Rectangle is : %lf",dRet);
+
+        getch();
+        return 0;
 }
