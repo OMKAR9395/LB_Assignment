6c6fec8429aef91b167edd8a21d50ff44fe0cd76
OMKAR9395 <omkarmohite85@gmail.com>
2024-10-16 17:29:08 +0530

    Assignment

---
+++ Assignment_8/01Number.c
@@ -0,0 +1,12 @@
+1.Write a program which accept range from user and display all numbers in between 
+that range.
+Input : 23 35 
+Output : 23 24 25 26 27 28 29 30 31 32 33 34 35
+Input : 10 18
+Output : 10 11 12 13 14 15 16 17 18
+Input : 10 10
+Output : 10
+Input : -10 2
+Output : -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2
+Input : 90 18
+Output : Invalid range
