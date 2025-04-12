1fcb2c7247f9c6e9cd462be3d5cb447f32514791
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-12 14:02:13 +0530

    Assignment

---
+++ Assignment_22/1.Write a program which accept string from user and count number of.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int CountCapital(char* str)
+{
+    int i = 0 , iCnt = 0;
+
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 65 && str[i] <= 90)
+        {
+            iCnt++;
+        }
+        i++;
+    }
+    return iCnt;
+}
+int main()
+{
+    char cArr[20] = "";
+    int iRet = 0;
+
+    printf("\nEnter the String : ");
+    scanf("%[^'\n']s",&cArr);
+
+    iRet = CountCapital(cArr);
+
+    printf("\n%d",iRet);
+
+    getch();
+    return 0;
+}
+
