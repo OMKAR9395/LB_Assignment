ba6f5f80774c2f877c6d4b1332376d4248c377ff
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-12 14:04:06 +0530

    Assignment

---
+++ Assignment_22/2. Write a program which accept string from user and count number of.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+///         int CountSmall(char str[])
+
+int CountSmall(char *str)
+{
+    int i = 0 , iCnt = 0;
+
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 97 && str[i] <= 122)
+        {
+            iCnt++;
+        }
+        i++;
+    }
+    return iCnt;
+}
+int main()
+{
+    char cArr[20] = "";
+    int iRet = 0;
+
+    printf("\nEnter the String : ");
+    scanf("%[^'\n']s",&cArr);
+
+    iRet = CountSmall(cArr);
+
+    printf("\n%d",iRet);
+
+    getch();
+    return 0;
+}
+
